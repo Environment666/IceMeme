e985a6d0b249021a3386158238defda57d3710f2
Environment666 <36128503+Environment666@users.noreply.github.com>
2018-02-04 03:50:44 -0800

    address updated for skids
    enjoy skids!!!

--- IceMeme/IceMeme/AddressandSSC.h
+++ IceMeme/IceMeme/AddressandSSC.h
@@ -1,17 +1,17 @@
 #pragma once
-DWORD getfieldaddress = ASLR(0x71d190);
-DWORD settopaddress = ASLR(0x71f2a0);
-DWORD pushstringaddress = ASLR(0x71e510);
-DWORD pushvalueaddress = ASLR(0x71e5d0);
-DWORD setfieldaddress = ASLR(0x71eec0);
-DWORD pushnumberaddress = ASLR(0x71e480);
-DWORD ScriptContextVFTableaddress = ASLR(0x69fd3e);
-DWORD getmetatableaddress = ASLR(0x71d460);
-DWORD pcalladdress = ASLR(0x71df50);
-DWORD rawrjzaddress = ASLR(0x7167a7);
-DWORD pushbooleanaddress = ASLR(0x71e020);
-DWORD Identityaddress = ASLR(0x1585DFC);
-DWORD pushniladdress = ASLR(0x71e410);
+DWORD getfieldaddress = ASLR(0x7219B0);
+DWORD settopaddress = ASLR(0x723AD0);
+DWORD pushstringaddress = ASLR(0x722D50);
+DWORD pushvalueaddress = ASLR(0x722E10);
+DWORD setfieldaddress = ASLR(0x7236F0);
+DWORD pushnumberaddress = ASLR(0x722CC0);
+DWORD ScriptContextVFTableaddress = ASLR(0x69EE5E);
+DWORD getmetatableaddress = ASLR(0x721CA0);
+DWORD pcalladdress = ASLR(0x722790);
+DWORD rawrjzaddress = ASLR(0x71B007);
+DWORD pushbooleanaddress = ASLR(0x722860);
+DWORD Identityaddress = ASLR(0x1585DFC); //IM NOT BOTHERING TO UPDATE THIS, DO IT URSELF
+DWORD pushniladdress = ASLR(0x71A730);
 
 int v41;//ScriptContext
 int v49 = 1;
@@ -21,6 +21,6 @@ void ScanScriptContext() {
 	DWORD ScriptContextVFTable = *(DWORD*)(ScriptContextVFTableaddress + 0x2);
 	v41 = Memory::Scan((char*)&ScriptContextVFTable);
 	//luaState
-	luaState = v41 + 56 * v49 + 164 - *(DWORD *)(v41 + 56 * v49 + 164);
-	*(DWORD*)Identityaddress = 6;
+	luaState = v41 + 56 * v49 + 220 + *(DWORD *)(v41 + 56 * v49 + 220);
+	//*(DWORD*)Identityaddress = 6; < commented cause address isnt updated
 }
